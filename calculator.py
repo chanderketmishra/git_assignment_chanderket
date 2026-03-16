2101481815155439b97674e548773eaaa51c43f8
Chanderket <chanderketmishra@gmail.com>
2026-03-16 14:48:26 +0530

    calculator code added

---
+++ calculator.py
@@ -0,0 +1,26 @@
+import math
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def square_root(self, x):
+        return math.sqrt(x)
+
+calculator = Calculator()
+
+print(calculator.add(10,5))
+print(calculator.subtract(10,5))
+print(calculator.multiply(10,5))
+print(calculator.divide(10,5))
+print(calculator.square_root(25))
